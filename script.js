fe2fb446e3d2456a711c5863a07ef7c43300eef4
Mayur Hanwate <70631103+lordbakyarou@users.noreply.github.com>
2023-09-17 19:03:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+
+let str =
+  "You are using " + navigator.appName + " version " + navigator.appVersion;
+
+let douc = document.getElementById("browser-info");
+
+douc.innerText = str;
